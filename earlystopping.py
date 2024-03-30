9f2319f317cac6689836d13746ff31eeb751b262
baconpancakess <baconpancakess@hotmail.com>
2024-03-30 08:20:52 -0300

    add earlystopping

---
+++ earlystopping.py
@@ -0,0 +1,39 @@
+#%%
+import copy
+
+#%%
+class EarlyStopping():
+    def __init__(self, patience = 5, threshold = 0, load_best_model = True, verbose = True):
+        self.patience = patience
+        self.threshold = threshold
+        self.load_best_model = load_best_model
+        self.verbose = verbose
+        self.best_model = None
+        self.best_loss = None
+        self.counter = 0
+        self.status = ""
+        
+    def __call__(self, model, current_loss):
+        if not self.best_loss:
+            self.best_loss = current_loss
+            self.best_model = copy.deepcopy(model)
+        
+        if self.best_loss - current_loss >= self.threshold:
+            self.counter = 0
+            self.best_loss = current_loss
+            self.best_model = self.best_model.load_state_dict(model.state_dict()) 
+        
+        if self.best_loss - current_loss < self.threshold:
+            self.counter += 1
+            
+            if self.counter >= self.patience:
+                print(f'Model stopped at counter = {self.counter}')
+                
+                if self.load_best_model:
+                    model.load_state_dict(self.best_model.state_dict())    
+                return False
+        
+        print(f'Current counter: {self.counter}/{self.patience}')  
+        return True
+        
+    
--- model.py
+++ model.py
@@ -7,15 +7,16 @@ import segmentation_models_pytorch as smp
 import graphs
 from train import*
 from dataset import*
+from earlystopping import EarlyStopping
 
 #%%
 DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
 
 # %%
 #Hyperparameters
-EPOCHS = 50
+EPOCHS = 500
 BATCH_SIZE = 3
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.001
 LOAD_MODEL = True       
 
 # %%
@@ -29,6 +30,7 @@ MODEL = smp.Unet(
 model = MODEL.to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
 loss = torch.nn.CrossEntropyLoss()
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, mode="min", factor=0.5, patience=5, threshold=0.001, cooldown=2, verbose=True )
 
 loss_hist = {
     "Train Loss": [],
@@ -48,42 +50,53 @@ if LOAD_MODEL:
         model_loader(model=model, path=f"{os.getcwd()}/saves/model/model_checkpoint.pth.tar")
     except FileNotFoundError:
         print("\nNo checkpoint was found...\n")
+
 # %%
-for epoch in range(EPOCHS):
-    train_dataset = SemanticDataset(f"path_to_train_images", f"path_to_train_masks")
-    tdata = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    
-    val_dataset = SemanticDataset(f"path_to_val_images", f"path_to_val_masks")
-    vdata = DataLoader(val_dataset, batch_size=BATCH_SIZE)
-    
-    train_loss = train_model(
-        dataloader = tdata, 
-        device = DEVICE,
-        model = model, 
-        loss = loss, 
-        optimizer = optimizer, 
-        epochs = EPOCHS, 
-        current_epoch = epoch, 
-        batch_size = BATCH_SIZE
-    )
-    
-    val_loss, val_accuracy = val_model(
-        dataloader = vdata, 
-        device = DEVICE,
-        model = model, 
-        loss = loss
-    )
-    
-    loss_hist["Train Loss"].append(train_loss)
-    loss_hist["Validation Loss"].append(val_loss)
-    
-    
-    print(f"""\nFor epoch[{epoch + 1}/{EPOCHS}]:
-          Train Loss: {train_loss:.4f}
-          Validation Loss: {val_loss:.4f}
-          Validation Accuracy: {val_accuracy:.2f}%\n
-          """)
-    
+es = EarlyStopping(threshold=0.001)
+
+# %%
+while train:
+    for epoch in range(EPOCHS):
+        train_dataset = SemanticDataset(f"/Users/luizfelipe/Desktop/Python/MachineLearning/Projects/SemanticSegmentation/train_images", f"/Users/luizfelipe/Desktop/Python/MachineLearning/Projects/SemanticSegmentation/train_masks")
+        tdata = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+        val_dataset = SemanticDataset(f"/Users/luizfelipe/Desktop/Python/MachineLearning/Projects/SemanticSegmentation/val_images", "/Users/luizfelipe/Desktop/Python/MachineLearning/Projects/SemanticSegmentation/val_masks")
+        vdata = DataLoader(val_dataset, batch_size=BATCH_SIZE)
+
+        train_loss = train_model(
+            dataloader = tdata, 
+            device = DEVICE,
+            model = model, 
+            loss = loss, 
+            optimizer = optimizer, 
+            epochs = EPOCHS, 
+            current_epoch = epoch, 
+            batch_size = BATCH_SIZE
+        )
+
+        val_loss, val_accuracy = val_model(
+            dataloader = vdata, 
+            device = DEVICE,
+            model = model, 
+            loss = loss
+        )
+
+        loss_hist["Train Loss"].append(train_loss)
+        loss_hist["Validation Loss"].append(val_loss)
+
+        scheduler.step(val_loss)
+        train = es(model=model, current_loss=val_loss)
+
+
+        print(f"""\nFor epoch[{epoch + 1}/{EPOCHS}]:
+              Train Loss: {train_loss:.4f}
+              Validation Loss: {val_loss:.4f}
+              Validation Accuracy: {val_accuracy:.2f}%\n
+              """)
+        
+        if not train:
+            break
+
 checkpoint = {"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
 model_saver(checkpoint=checkpoint, path=f"{os.getcwd()}/saves/model/model_checkpoint_{EPOCHS}_BCELoss.pth.tar")
 
